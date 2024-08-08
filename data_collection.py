3521f4570c556ea111a7ac571607bde98581cbb6
Bryan <bryanlondres5@gmail.com>
2024-08-08 09:53:08 -0400

    Updated fetch_news function to use provided APIkey

--- data_collection.py
+++ data_collection.py
@@ -2,12 +2,10 @@
 
 import requests
 
-def fetch_news(stock):
-    api_key = 'YOUR_NEWS_API_KEY'  # Replace with your actual news API key
+def fetch_news(stock, api_key):
     url = f'https://newsapi.org/v2/everything?q={stock}&apiKey={api_key}'
-    
     response = requests.get(url)
     data = response.json()
-    
     return data
+
 
