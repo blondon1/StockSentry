e2cc6751f23b8d99cf5681a6a08339b3835ea2ed
Bryan <bryanlondres5@gmail.com>
2024-08-08 09:54:39 -0400

    Updated fetch_current_price function to use provided API key

--- stock_price.py
+++ stock_price.py
@@ -2,8 +2,7 @@
 
 import requests
 
-def fetch_current_price(stock):
-    api_key = 'YOUR_ALPHA_VANTAGE_API_KEY'  # Replace with your Alpha Vantage API key
+def fetch_current_price(stock, api_key):
     url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock}&apikey={api_key}'
     
     response = requests.get(url)
